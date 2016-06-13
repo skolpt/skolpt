0e0212a404a3fb654259e53c3701e913800f3b4c
skolpt <skolpt.aplabs@gmail.com>
2016-06-13 12:03:57 +0000

    updated carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      :provider => 'fog/aws',
+      :provider => 'AWS',
       # Heroky uses the ENV keys to set up the environment
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
