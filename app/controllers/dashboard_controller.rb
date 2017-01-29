9d300831604cdad0c91b4d828d8c4b846eb866fc
skolpt <skolpt.aplabs@gmail.com>
2017-01-29 04:53:18 +0000

    Cleaned up the user profile page, in preparation for future updates

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   
   def index
     if !current_athlete.nil? && session[:athlete_id] == current_athlete.id && current_athlete and current_athlete.id == params[:id]
-      flash[:success] = "Welcome back " + current_athlete.email+"!"
+      #flash[:success] = "Welcome back " + current_athlete.email+"!"
     else
       flash[:danger] = "You are not logged in"
       redirect_to root_path
@@ -12,7 +12,7 @@ class DashboardController < ApplicationController
     @athlete = Athlete.find(params[:id])
     
     if !current_athlete.nil?
-      flash[:success] = "Welcome back " + current_athlete.email+"!"
+      #flash[:success] = "Welcome back " + current_athlete.email+"!"
     else
       flash[:danger] = "Not allowed, are have you logged in?"
       redirect_to root_path
