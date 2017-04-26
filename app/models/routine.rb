39ffa3621a94a06481fa356d5dc01a8a530089de
skolpt <skolpt.aplabs@gmail.com>
2017-04-26 07:06:05 +0000

    Added validations to routine creation

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -5,4 +5,9 @@ class Routine < ActiveRecord::Base
   
   accepts_nested_attributes_for :routine_exercises, :allow_destroy => true
   accepts_nested_attributes_for :exercises
+  
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :difficulty, presence: true
+  validates :description, presence: true, length: { minimum: 20, maximum: 500 }
+  
 end
--- app/models/routine_exercise.rb
+++ app/models/routine_exercise.rb
@@ -3,4 +3,7 @@ class RoutineExercise < ActiveRecord::Base
   belongs_to :exercise
   
   accepts_nested_attributes_for :exercise
+  
+  validates :sets, presence: true, length: { maximum: 4 }
+  validates :reps, presence: true, length: { maximum: 4 }
 end
